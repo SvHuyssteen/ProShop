68fa153d7233c03638968ef59901396aad53f6dd
Shaun van Huyssteen <littleblueline@gmail.com>
2020-10-07 22:09:13 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <main className='py-3'>
+          <h1>Welcome to ProShop</h1>
+        </main>
+      </Container>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;

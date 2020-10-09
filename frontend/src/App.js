4c338bbfe0072bac9de6726bae6b83a6b8fc951d
Shaun van Huyssteen <littleblueline@gmail.com>
2020-10-09 07:23:00 +0200

    Just finished the cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./Screens/HomeScreen";
 function App() {
   return (
     <>
       <Header />
       <Container>
         <main className='py-3'>
-          <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </main>
       </Container>
 

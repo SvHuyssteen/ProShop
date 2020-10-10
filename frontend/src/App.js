df1a947a036979ed86bb88fb477fb43191fe7f4a
Shaun van Huyssteen <littleblueline@gmail.com>
2020-10-10 11:01:45 +0200

    got the skeleton in for the product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Container>
-        <main className='py-3'>
-          <HomeScreen />
-        </main>
-      </Container>
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
 
       <Footer />
-    </>
+    </Router>
   );
 }
 
